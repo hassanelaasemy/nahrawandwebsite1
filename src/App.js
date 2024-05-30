0cf75e64e550eb9e2c0e7903c208144a624a36bf
khalidmk1 <103368578+khalidmk1@users.noreply.github.com>
2024-05-30 13:30:39 +0100

    create navbar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
-import "bootstrap/dist/css/bootstrap.css";
-import SliderComming from "./pages/slider/SliderComming";
+import Navbar from "./components/Navbar";
+
+/* import SliderComming from "./pages/slider/SliderComming"; */
 function App() {
   return (
     <div className="App">
-      <SliderComming />
+
+      <Navbar />
     </div>
   );
 }
