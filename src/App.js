c9714ce3e7e3b72522a0c52e15e9401ff9dcdaf8
hassanelaasemy <elaasemyhassan01@gmail.com>
2024-05-30 12:44:52 +0200

    nahrawand academy website

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
-import logo from './logo.svg';
-import './App.css';
-
+import logo from "./logo.svg";
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.css";
 function App() {
   return (
     <div className="App">
@@ -9,6 +9,9 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <button type="button" class="btn btn-primary">
+          Primary
+        </button>
         <a
           className="App-link"
           href="https://reactjs.org"
