9365ebab7c8bbdd95dec33070a1a2f7332edfe51
hassanelaasemy <elaasemyhassan01@gmail.com>
2024-05-31 12:45:23 +0200

    nahrawand academy new application

--- src/App.js
+++ src/App.js
@@ -3,13 +3,17 @@ import "./App.css";
 import SliderComming from "./pages/slider/SliderComming";
 import Navbar from "./components/Navbar";
 import FormationSection from "./pages/formation/FormationSection";
+import Eventsection from "./pages/event/Eventsection";
+import SpeakersSection from "./pages/speakers/SpeakersSection";
 
 function App() {
   return (
     <div className="App">
       <Navbar />
       <SliderComming />
-      <FormationSection/>
+      <FormationSection />
+      <Eventsection />
+      <SpeakersSection />
     </div>
   );
 }
