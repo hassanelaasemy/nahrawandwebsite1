91f501c841c1fa2f2561f48b888e0d35cebdecc5
khalidmk1 <103368578+khalidmk1@users.noreply.github.com>
2024-05-31 10:13:09 +0100

    create formation section

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
 import "./App.css";
 
 import SliderComming from "./pages/slider/SliderComming";
-
-
 import Navbar from "./components/Navbar";
+import FormationSection from "./pages/formation/FormationSection";
 
 function App() {
   return (
     <div className="App">
-
+      <Navbar />
       <SliderComming />
+      <FormationSection/>
     </div>
   );
 }
